2015e06702500a505b98e8713be585deeb8a7433
Alen Basic <admin@alenbasic.com>
2016-07-21 22:49:49 +1000

    Merge model.go and go-player.go files
    Also extends the renderTemplate function to use other structs other
    than PageData

--- go-player.go
+++ go-player.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
 )
 
 // GLOBALS DECLARED HERE
@@ -13,6 +18,51 @@ var tmplDir = "./templates/"
 var templates map[string]*template.Template
 var pageData = PageData{}
 var firstStart = true
+
+// THE MODEL CODE IS HERE
+
+type Movie struct {
+	FullFilePath string
+	FileName     string
+}
+
+type PageData struct {
+	MovieList   []Movie
+	CurrentFilm string
+	Player      Player
+}
+
+// LOOKS FOR FILES ON THE FILESYSTEM
+
+var extensionList = [][]byte{
+	{'.', 'm', 'k', 'v'},
+	{'.', 'm', 'p', 'g'},
+	{'.', 'a', 'v', 'i'},
+	{'.', 'm', '4', 'v'},
+	{'.', 'm', 'p', '4'}}
+
+func visit(path string, f os.FileInfo, err error) error {
+	bpath := []byte(strings.ToLower(path))
+	bpath = bpath[len(bpath)-4:]
+	for i := 0; i < len(extensionList); i++ {
+		if reflect.DeepEqual(bpath, extensionList[i]) {
+			movie := Movie{path, f.Name()}
+			pageData.MovieList = append(pageData.MovieList, movie)
+		}
+	}
+	return nil
+}
+
+func generateMovies(path string) error {
+	err := filepath.Walk(path, visit)
+	if err != nil {
+		return err
+	} else if len(pageData.MovieList) <= 0 {
+		return fmt.Errorf("No media files were found in the given path: %s", path)
+	}
+	fmt.Printf("file import complete: %d files imported\n", len(pageData.MovieList))
+	return err
+}
 
 // THE VIEW CODE IS HERE
 
@@ -26,8 +76,14 @@ func generateTemplates() {
 	}
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	err := templates[tmpl].Execute(w, pageData)
+func renderTemplate(pageStruct interface{}, w http.ResponseWriter, tmpl string) {
+	var err error
+	if pageStruct == nil {
+		err = templates[tmpl].Execute(w, pageData)
+	} else {
+		err = templates[tmpl].Execute(w, pageStruct)
+	}
+
 	if err != nil {
 		log.Printf("The follwing error occurred: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -73,11 +129,11 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 	}
-	renderTemplate(w, tmpl)
+	renderTemplate(nil, w, tmpl)
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.html")
+	renderTemplate(nil, w, "about.html")
 }
 
 func setupHandler(w http.ResponseWriter, r *http.Request) {
@@ -96,7 +152,7 @@ func setupHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	renderTemplate(w, tmpl)
+	renderTemplate(nil, w, tmpl)
 }
 
 func movieHandler(w http.ResponseWriter, r *http.Request) {
@@ -134,7 +190,7 @@ func movieHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusFound)
 		return
 	}
-	renderTemplate(w, "movie.html")
+	renderTemplate(nil, w, "movie.html")
 }
 
 // IT ALL STARTS HERE
